3af7c873bf10d7bb5f8cb7446ee1207727b50fd6
Emilio Moreno <emiliomoremil@gmail.com>
2023-06-20 16:52:51 +0200

    conditional rendering a, b, c

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,8 @@ function Main() {
 
     const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
     
+
+    
     const deleteMovie = (movieId) => {
         console.log("deleting movie with id....", movieId)
 
@@ -21,12 +23,37 @@ function Main() {
 
     }
 
+     // Conditional Rendering: option A (Element Variables)
+    let message = "";
+    if(moviesToDisplay.length > 0){
+        message = <h1>Number of movies: {moviesToDisplay.length}</h1>;
+    } else {
+        message = <h1>Sorry, no movies to display</h1>;
+    }
+
+
     return (
         <div className="Main">
+{/* this  {message} comes from the conditional rendering option A */}
+            {message} 
+
             {moviesToDisplay.map((movieObj) => {
                 return(
                     <div key={movieObj.id} className="card">
                         <p>{movieObj.title}</p>
+                        <p>Rating: {movieObj.rating}</p>
+
+                        {/* conditional rendering option C */}
+                        {movieObj.imgURL                                             //if this confition is truthy, then show our image
+                        ? <img src={movieObj.imgURL} />                             //if the conditoin is not truthy... then show the placeholder
+                        : <img src="https://dummyimage.com/182x268/ffffff/000000" /> //this is a generic placeholder icon from the internet
+                         }
+                        
+
+                        {/* OPTION B //  if the rating is above 8, then render a message> RECCOMENDED */}
+                        { movieObj.rating > 8 && <p>RECOMMENDED</p>}
+
+
                         <button onClick={ () => {deleteMovie(movieObj.id)}}>Delete this movie</button>
                     </div>
                 )
