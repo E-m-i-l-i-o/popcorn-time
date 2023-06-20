558f6cf14e9fea682f1cadd3ffc0920007389f94
Emilio Moreno <emiliomoremil@gmail.com>
2023-06-20 14:27:12 +0200

    first

---
+++ src/components/Main.js
@@ -0,0 +1,32 @@
+function Main() {
+
+    const moviesArray = [
+        {
+            "id": 1,
+            "title": "The Godfather",
+            "rating": 9
+        },
+        {
+            "id": 2,
+            "title": "Forrest Gump",
+            "rating": 9
+        },
+        {
+            "id": 3,
+            "title": "Gladiator",
+            "rating": 8
+        },
+    ]
+
+    return (
+        <div className="Main">
+            {moviesArray.map(function(){
+
+            })}
+
+
+        </div>
+    );
+}
+
+export default Main;
