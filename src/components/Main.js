908e61cc867cb69545fb1db4d6c588af46a304aa
Emilio Moreno <emiliomoremil@gmail.com>
2023-06-20 16:03:01 +0200

    delete movies fucntion working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,36 @@
+import { useState } from "react";
+import movies from "../data/movies.json";
+
+import "./Main.css";
+
 function Main() {
 
-    const moviesArray = [
-        {
-            "id": 1,
-            "title": "The Godfather",
-            "rating": 9
-        },
-        {
-            "id": 2,
-            "title": "Forrest Gump",
-            "rating": 9
-        },
-        {
-            "id": 3,
-            "title": "Gladiator",
-            "rating": 8
-        },
-    ]
+
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+    
+    const deleteMovie = (movieId) => {
+        console.log("deleting movie with id....", movieId)
+
+        // moviesToDisplay.push(); // NEVER MODIFY STATE DIRECTLY !
+
+        const newList = moviesToDisplay.filter( (element) => {    //.filter returns an array. I store it in newList
+            return element.id !== movieId;                          //here we tell the fucntion: only pass to the final array all those elements that are NOT the one I click
+        });
+
+        setMoviesToDisplay(newList);                                //we call this fucntion passing the final array
+
+    }
 
     return (
         <div className="Main">
-            {moviesArray.map(function(){
-
+            {moviesToDisplay.map((movieObj) => {
+                return(
+                    <div key={movieObj.id} className="card">
+                        <p>{movieObj.title}</p>
+                        <button onClick={ () => {deleteMovie(movieObj.id)}}>Delete this movie</button>
+                    </div>
+                )
             })}
-
-
         </div>
     );
 }
